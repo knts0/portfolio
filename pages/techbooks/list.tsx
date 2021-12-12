982f088885fc073bb0abc8b15a023e99bb73d5a4
Kana Otawara <shimmer22.0204@gmail.com>
2021-12-12 23:50:35 +0900

    use footer & remove unused css

--- pages/techbooks/list.tsx
+++ pages/techbooks/list.tsx
@@ -9,6 +9,7 @@ import {
 import Link from 'next/link'
 
 import Header from '../../components/header'
+import Footer from '../../components/footer';
 import { getTechbookList } from '../../fetchData/getTechbookData';
 
 export type Techbook = {
@@ -83,92 +84,7 @@ const SSRPage = ({ data }) => {
         </Box>
       </Box>
 
-      <footer>
-        <div>© Kana Otawara 2021</div>
-        {/* <img src="/vercel.svg" alt="Vercel" className="logo" /> */}
-        <div>
-
-        </div>
-      </footer>
-
-      <style jsx>{`
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <Footer></Footer>
     </div>
   )
 }
